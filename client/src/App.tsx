a2c4d93c354e10ad73b1adc8bd4413fe71a720f2
Ajeet Sinha <honeydreamssoftwares@gmail.com>
2024-04-23 02:29:20 +0530

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,20 @@
+//import 'react-toastify/dist/ReactToastify.css';
+
+import './App.css'
+import MoveEngine from './components/MoveEngine'
+
+function App() {
+
+
+  return (
+    <>
+
+      <p className="read-the-docs">
+        Simple Multiplayer Chess
+      </p>
+      <MoveEngine></MoveEngine>
+    </>
+  )
+}
+
+export default App
