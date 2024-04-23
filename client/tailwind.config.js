94883bbb1b7baa318f08702e3a8370d3f4fb47c8
Ajeet Sinha <honeydreamssoftwares@gmail.com>
2024-04-23 23:47:19 +0530

    added tailwind

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'],
+  darkMode: false,
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
