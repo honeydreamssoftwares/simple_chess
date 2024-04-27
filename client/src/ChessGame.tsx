81da1efe9dc499f300b70ee7fca44c6102430916
Ajeet Sinha <honeydreamssoftwares@gmail.com>
2024-04-27 15:15:18 +0530

    state refactoring

--- client/src/ChessGame.tsx
+++ client/src/ChessGame.tsx
@@ -8,7 +8,7 @@ import MoveHistory from "./MoveHistory";
 import "./App.css";
 
 //Import server states
-import type { ChessRoomState } from "../../server/src/rooms/schema/ChessRoomState"; 
+import type { ChessRoomState } from "../../server/src/rooms/schema/ChessRoomState";
 import type PlayerMove from "../../server/src/rooms/schema/PlayerMove";
 
 import { ArraySchema } from "@colyseus/schema";
@@ -19,9 +19,7 @@ function ChessGame() {
   );
   const [room, setRoom] = useState<Colyseus.Room<ChessRoomState>>();
   const [error, setError] = useState("");
-  const [fen, setFen] = useState(
-    "start"
-  );
+  const [fen, setFen] = useState("start");
 
   const [playerCount, setPlayerCount] = useState(0);
   const [isWhite, setIsWhite] = useState(true); // True if this client plays as White
@@ -45,7 +43,7 @@ function ChessGame() {
         toast.info("Waiting for player..");
       });
 
-/*       room.onMessage("game_over", (message) => {
+      /*       room.onMessage("game_over", (message) => {
         setGameOver(true);
         setGameResult(`${message.status} - Winner: ${message.winner}`);
         toast.info(`Game Over: ${message.status} - Winner: ${message.winner}`);
@@ -60,60 +58,63 @@ function ChessGame() {
       room.state.listen("turn_of_player", (currentValue) => {
         console.log(`current value is now ${currentValue}`);
         setTurn(currentValue);
-    });
+      });
 
-    room.state.listen("number_of_players", (currentValue) => {
-      console.log(`current value is now ${currentValue}`);
-      setPlayerCount(currentValue);
-  });
+      room.state.listen("number_of_players", (currentValue) => {
+        console.log(`current value is now ${currentValue}`);
+        setPlayerCount(currentValue);
+      });
 
-  room.state.listen("fen", (currentValue) => {
-    console.log(`current value is now ${currentValue}`);
-    setFen(currentValue);
-});
+      room.state.listen("fen", (currentValue) => {
+        console.log(`current value is now ${currentValue}`);
+        setFen(currentValue);
+      });
 
-room.state.listen("moves", (currentValue) => {
-  console.log(`current value is now ${currentValue}`);
-  setMoves(new ArraySchema<PlayerMove>(...currentValue));
-});
+      room.state.listen("moves", (currentValue) => {
+        console.log(`current value is now ${currentValue}`);
+        setMoves(new ArraySchema<PlayerMove>(...currentValue));
+      });
 
+      room.state.listen("number_of_players", (currentValue) => {
+        console.log(`current value is now ${currentValue}`);
 
-      room.onStateChange((state) => {
-        console.log(room.name, "has new state:", state);
-       // setFen(state.fen);
-        //setMoves(new ArraySchema<PlayerMove>(...state.moves));
-        
-        //setPlayerCount(state.number_of_players);
-
-        //Opponent name
-        if (state.number_of_players === 2) {
-          state.players.forEach((details, sessionId) => {
-            if (sessionId !== room.sessionId) {
-              setOpponentName(details.name);
-            }
-          });
-        }
-
-        // Own colour
-        state.players.forEach((details, sessionId) => {
+        //Self details
+        room.state.players.forEach((details, sessionId) => {
           if (sessionId === room.sessionId) {
             setIsWhite(details.color === "white");
             setPlayerColor(details.color);
           }
         });
 
+
+        //Opponent name
+        if (currentValue === 2) {
+          room.state.players.forEach((details, sessionId) => {
+            if (sessionId !== room.sessionId) {
+              setOpponentName(details.name);
+            }
+          });
+        }
+      });
+
+      room.state.listen("is_game_running", (currentValue) => {
+        console.log(`current value is now ${currentValue}`);
         //Game over
-        if(state.is_game_running===false){
+        if (currentValue=== false) {
           //Game is over now
           setGameOver(true);
           //Since draw has no winner
-          let winner="No One";
-          if(state.game_result_winner !== ""){
-            winner=state.game_result_winner;
+          let winner = "No One";
+          if (room.state.game_result_winner !== "") {
+            winner = room.state.game_result_winner;
           }
-          setGameResult(state.game_result_status+"  Winner:"+winner);
+          setGameResult(room.state.game_result_status + "  Winner:" + winner);
         }
       });
+
+
+
+
     }
   }, [room]);
 
@@ -123,9 +124,12 @@ room.state.listen("moves", (currentValue) => {
       return;
     }
     try {
-      const joinedRoom = await client.joinOrCreate<ChessRoomState>("chess_room", {
-        playerName,
-      });
+      const joinedRoom = await client.joinOrCreate<ChessRoomState>(
+        "chess_room",
+        {
+          playerName,
+        }
+      );
       console.log(joinedRoom.sessionId, "joined", joinedRoom.name);
       setRoom(joinedRoom);
     } catch (e) {
